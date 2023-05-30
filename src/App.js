52fa7988a33d98dc0561413704df59c4245c90d8
yih1612 <yih1612@naver.com>
2023-05-30 09:59:39 +0900

    12.8 video - Mock data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import { Outlet } from "react-router-dom";
 import SearchHeader from "./components/SearchHeader";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
       <SearchHeader />
-      <Outlet />
+      {/* 네트워크 통신이 필요한 곳에 감싸준다 */}
+      <QueryClientProvider client={queryClient}>
+        <Outlet />
+      </QueryClientProvider>
     </>
   );
 }
