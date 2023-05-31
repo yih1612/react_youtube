a6dee0c440ec3a6a54060a7ad2634fc8ffcc6575
yih1612 <yih1612@naver.com>
2023-05-31 15:55:29 +0900

    12.16 video card - style

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -5,12 +5,11 @@ export default function VideoCard({ video }) {
   const { title, thumbnails, channelTitle, publishedAt } = video.snippet;
   return (
     <li>
-      <img src={thumbnails.medium.url} alt={title} />
+      <img className="w-full" src={thumbnails.medium.url} alt={title} />
       <div>
-        <p>{title}</p>
-        <p>{channelTitle}</p>
-        <p>{publishedAt}</p>
-        {formatAgo(publishedAt)}
+        <p className="font-semibold my-2 line-clamp-2">{title}</p>
+        <p className="text-sm opacity-80">{channelTitle}</p>
+        <p className="text-sm opacity-80">{formatAgo(publishedAt)}</p>
       </div>
     </li>
   );
