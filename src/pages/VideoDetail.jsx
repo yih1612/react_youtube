5679f7757d60046d55eef11e727ef5f4469206cc
yih1612 <yih1612@naver.com>
2023-05-31 17:33:49 +0900

    12.18 viewDetail - frame

--- src/pages/VideoDetail.jsx
+++ src/pages/VideoDetail.jsx
@@ -1,5 +1,31 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
+import ChannelInfo from "../components/ChannelInfo";
+import RelatedVideos from "../components/RelatedVideos";
 
 export default function VideoDetail() {
-  return <h2>VideoDetail</h2>;
+  const {
+    state: { video },
+  } = useLocation();
+  const { title, channelId, channelTitle, description } = video.snippet;
+  return (
+    <section>
+      <article>
+        <iframe
+          id="player"
+          type="text/html"
+          width="100%"
+          height="640"
+          src={`http://www.youtube.com/embed/${video.id}`}
+          frameBorder="0"
+        />
+        <h2>{title}</h2>
+        <ChannelInfo id={channelId} name={channelTitle} />
+        <pre>{description}</pre>
+      </article>
+      <section>
+        <RelatedVideos id={video.id} />
+      </section>
+    </section>
+  );
 }
